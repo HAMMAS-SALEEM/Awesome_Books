afb8fd2fb7650485a5b2a8df7c343af1594b8f4c
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-04 19:24:47 +0500

    Update script for project

--- script.js
+++ script.js
@@ -4,52 +4,44 @@ let addBtn = document.querySelector('.addBtn')
 let removeBtn = document.querySelector('#removeBtn')
 let output = document.querySelector('.list-elements')
 let arr = [];
-
-
-
-
 addBtn.addEventListener('click', () => {
     pushBook();
     pushListItem();
 }
 )
-
 function pushBook() {
-    arr.unshift({
-        id: new Date().getTime().toString(),
-        book : bookName.value,
-        author : author.value
+    if (bookName.value !== "" && author.value !== "") {
+        arr.unshift({
+            id: new Date().getTime().toString(),
+            book : bookName.value,
+            author : author.value
+        });
+        locStorage();
     }
-    )
-    
-    locStorage();
 }
-
 function pushListItem() {
     let bookHtml = '';
-    let booksArray = localStorage.getItem('booksList');
-    let booksItems = JSON.parse(booksArray);
-    if(booksItems==null){
-        
-    } else {
-    booksItems.forEach(item=>{
-    bookHtml += `<li class="list_item"><p class="book-name">${item.book}</p><br>
-    <p class="book-author">${item.author}</p>
-<button type="button" id=${item.id} onclick="removeData(this.id)">Remove</button>
-<hr class="line"></li>`
-    })
-    output.innerHTML = bookHtml
-}
-
+    let booksArray = JSON.parse(localStorage.getItem('booksList'));
+    if(booksArray!==null){
+        arr = booksArray;
+        let i = 0;
+        booksArray.forEach((item)=>{
+            console.log(i);
+            i++;
+        bookHtml += `<li class="list_item"><p class="book-name">${item.book}</p><br>
+                        <p class="book-author">${item.author}</p>
+                    <button type="button" id=${item.id} onclick="removeData(this.id)">Remove</button>
+                    <hr class="line"></li>`
+        });
+        output.innerHTML = bookHtml
+    }
 }
 pushListItem();
-
 function removeData(id) {
     arr = arr.filter(e=> e.id.toString() !== id.toString())
     locStorage();
     pushListItem();
 }
-
 function locStorage() {
     localStorage.setItem("booksList" , JSON.stringify(arr))
 }
